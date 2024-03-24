356a4e36fb12c699a740ddf14204ed053b6572fc
joelquispe <joelsosayaquispe@gmail.com>
2024-03-24 14:34:10 -0500

    refactor: createHashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,8 @@
 import './App.css';
-import { FaGithub, FaGitlab } from 'react-icons/fa';
 
 function App() {
 	return (
-		<div className="min-h-screen w-screen  bg-background">
-			<div id="navbar" className="absolute text-2xl top-3 left-3 flex gap-x-4">
-				<div className="text-white"></div>
-				<div className="text-white">
-					<FaGithub />
-				</div>
-				<div className="text-white">
-					<FaGitlab />
-				</div>
-			</div>
-			{/* <div
-				id="footer"
-				className="absolute bottom-0 border-t border-gray-400 w-screen px-2 py-3 flex justify-around bg-black"
-			>
-				<div className="w-min">
-					<IoHome className="text-white text-xl m-auto mb-1" />
-					<p className="text-white text-md">Home</p>
-				</div>
-				<div className="w-min">
-					<MdWebAsset className="text-white text-xl m-auto mb-1" />
-					<p className="text-white text-md">Web</p>
-				</div>
-				<div className="w-min">
-					<FaMobileAlt className="text-white text-xl m-auto mb-1" />
-					<p className="text-white text-md">Mobile</p>
-				</div>
-				<div className="w-min  focus:transition-all ease-out focus:duration-75">
-					<MdOutlineWorkHistory className="text-white text-xl m-auto mb-1" />
-					<p className="text-white text-md">Experience</p>
-				</div>
-			</div> */}
-			{/* <TypeAnimation
-        sequence={[
-          // Same substring at the start will only be typed out once, initially
-          "Joel Alexander Sosaya Quispe - Desarrollador Full Stack",
-        ]}
-        wrapper="span"
-        speed={50}
-        className="text-white "
-        style={{ fontSize: "2em", display: "inline-block" }}
-        cursor={false}
-        repeat={Infinity}
-      /> */}
-		</div>
+		<></>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import HomePage from './ui/pages/home/home.page.tsx';
 import ProjectsPage from './ui/pages/projects/projects.page.tsx';
 import ContactPage from './ui/pages/contact/contact.page.tsx';
 import MainLayout from './ui/layouts/main.layout.tsx';
 import TechnologiesPage from './ui/pages/technologies/technologies.page.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 	{
 		element: <MainLayout />,
 		
